0cb6d59cb5d9c7322edb00a556f89171a2d5c448
Vondigold <honoriosaenz655@gmail.com>
2025-02-12 20:56:55 -0600

    Update sprite, width, heigth.

--- Content/Items/Fortune.cs
+++ Content/Items/Fortune.cs
@@ -12,16 +12,16 @@ namespace APCompSciPT.Content.Items
 		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.APCompSciPT.hjson' file.
 		public override void SetDefaults()
 		{
-			Item.damage = 50;
+			Item.damage = 25;
 			Item.DamageType = DamageClass.Melee;
-			Item.width = 40;
-			Item.height = 40;
+			Item.width = 80;
+			Item.height = 80;
 			Item.useTime = 20;
 			Item.useAnimation = 20;
 			Item.useStyle = ItemUseStyleID.Swing;
 			Item.knockBack = 6;
-			Item.value = Item.buyPrice(gold: 8, silver: 32)
-			Item.rare = ItemRarityID.Blue;
+			Item.value = Item.buyPrice(gold: 8, silver: 32);
+			Item.rare = ItemRarityID.LightRed;
 			Item.UseSound = SoundID.Item1;
 			Item.autoReuse = true;
 		}
