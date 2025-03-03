7e7f93aaa1b041aee76b63c9c1d2e984e55a08d8
Cheesium Incorporated <honoriosaenz655@gmail.com>
2025-03-03 15:46:56 -0600

    Fixed an error that happened when the branches merged.

--- APCompSciPT/Content/Items/Fortune.cs
+++ APCompSciPT/Content/Items/Fortune.cs
@@ -46,24 +46,24 @@ namespace APCompSciPT.Content.Items
             recipe.Register();
         }
         
-         //Checks if the weapon dealt damage to a enemy.
+               //Checks if the weapon dealt damage to a enemy.
         public override void OnHitNPC(Player player, NPC target, NPC.HitInfo hit, int damageDone)
         {
+            //Arrays for buffs and debuffs that can be used. Buffs can be found in https://terraria.fandom.com/wiki/Buff_IDs
             int[] buffArray = new int[] { 1, 2, 3, 5, 6, 7, 14, 16, 26, 58, 71, 73, 74, 75, 76, 77, 78, 79, 87, 89, 93, 97, 100, 105, 112, 113, 114, 116, 115, 117, 146, 151, 150, 158, 205, 215 };
             int[] debuffArray = new int[] { 30, 20, 24, 70, 22, 80, 35, 23, 31, 32, 33, 36, 39, 69, 44, 144, 21, 94, 67, 120, 353, 153 };
             
             gambleFever(rnd.Next(0,100))
 
+            //Picks a random number for hostile NPC chance of a buff/Debuff.
             int chance = rnd.Next(0, 100);
+            //80% Chance for a debuff to a hostile NPC.
             if (0 <= chance && chance <= 80)
             {
+                //Gives the hostile NPC a debuff/s.
                 for (int i = 0; i < rnd.Next(1, 5); i++)
                 {
                     int gamble = rnd.Next(0, debuffArray.Length - 1);
-                    target.AddBuff(debuffArray[gamble], rnd.Next(360, 720)); 
-            }
-            
-            if (gambleFever >= 5)
                     target.AddBuff(debuffArray[gamble], rnd.Next(360, 720)); //A second is the random number divided by 60.
                 }
             }
@@ -74,10 +74,11 @@ namespace APCompSciPT.Content.Items
             //checks if the gamble count 
              if (gambleChance >= 40)
             {
+                //Gives the player a buff/s.
                 for (int i = 0; i < rnd.Next(1, 5); i++)
                 {
+                    //Picks a random index from buffArray to give a buff.
                     int gamble = rnd.Next(0, buffArray.Length - 1);
-                    player.AddBuff(buffArray[gamble], rnd.Next(360, 720)); 
                     player.AddBuff(buffArray[gamble], rnd.Next(360, 720)); //A second is the random number divided by 60.
                 }
             }
