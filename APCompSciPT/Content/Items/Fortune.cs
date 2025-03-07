9e7ea5535f29e8e7b2d3466d421412e0baab92ff
Cheesium Incorporated <honoriosaenz655@gmail.com>
2025-03-07 07:55:19 -0600

    Attemtping to fix code

--- APCompSciPT/Content/Items/Fortune.cs
+++ APCompSciPT/Content/Items/Fortune.cs
@@ -6,7 +6,7 @@ using System;
 
 namespace APCompSciPT.Content.Items
 { 
-    public class Fortune : ModItem
+public class Fortune : ModItem
     {
         //Sets the way random numbers are generated from Using System.
         Random rnd = new Random();
@@ -45,15 +45,15 @@ namespace APCompSciPT.Content.Items
             recipe.AddTile(TileID.Anvils);
             recipe.Register();
         }
-        
-               //Checks if the weapon dealt damage to a enemy.
+
+        //Checks if the weapon dealt damage to a enemy.
         public override void OnHitNPC(Player player, NPC target, NPC.HitInfo hit, int damageDone)
         {
             //Arrays for buffs and debuffs that can be used. Buffs can be found in https://terraria.fandom.com/wiki/Buff_IDs
             int[] buffArray = new int[] { 1, 2, 3, 5, 6, 7, 14, 16, 26, 58, 71, 73, 74, 75, 76, 77, 78, 79, 87, 89, 93, 97, 100, 105, 112, 113, 114, 116, 115, 117, 146, 151, 150, 158, 205, 215 };
             int[] debuffArray = new int[] { 30, 20, 24, 70, 22, 80, 35, 23, 31, 32, 33, 36, 39, 69, 44, 144, 21, 94, 67, 120, 353, 153 };
 
-            gambleFever(rnd.Next(0, 100));
+            gambleFever(player, buffArray, rnd.Next(0, 100));
 
             //Picks a random number for hostile NPC chance of a buff/Debuff.
             int chance = rnd.Next(0, 100);
